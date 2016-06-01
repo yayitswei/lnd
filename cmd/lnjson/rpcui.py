aedbe2f885cec01c8159a4e32ec66e133e2a3ec4
Tadge Dryja <tadge@lightning.network>
2016-05-31 18:38:58 -0700

    add more rpc ui

--- cmd/lnjson/rpcui.py
+++ cmd/lnjson/rpcui.py
@@ -9,15 +9,44 @@ import gtk
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # global for socket connection
 
+def getBal():
+	rpcCmd = {
+		   "method": "LNRpc.Bal",
+		   "params": [{
+	   }]
+	}
+	rpcCmd.update({"jsonrpc": "2.0", "id": "99"})
+	print(json.dumps(rpcCmd))
+	s.sendall(json.dumps(rpcCmd))
+	r = json.loads(s.recv(8000000))
+	print(r)
+	return r["result"]["TotalScore"]
+
+def getAdr():
+	rpcCmd = {
+		   "method": "LNRpc.Address",
+		   "params": [{
+	   "NumToMake": 0,
+	   }]
+	}
+	rpcCmd.update({"jsonrpc": "2.0", "id": "99"})
+	print(json.dumps(rpcCmd))
+	s.sendall(json.dumps(rpcCmd))
+	r = json.loads(s.recv(8000000))
+	print(r)
+	n = len(r["result"]["PreviousAddresses"]) -1
+	return r["result"]["PreviousAddresses"][n]   #[len(r["result"]["PreviousAddresses"]-1)]
+	
 def prSend(adr, amt):
-	rpc_input = {
+	rpcCmd = {
 		   "method": "LNRpc.Send",
 		   "params": [{"DestAddrs": [adr,],"Amts": [amt,]}]
 	}
-	rpc_input.update({"jsonrpc": "2.0", "id": "99"})
-	print(json.dumps(rpc_input))
-	s.sendall(json.dumps(rpc_input))
+	rpcCmd.update({"jsonrpc": "2.0", "id": "99"})
+	print(json.dumps(rpcCmd))
+	s.sendall(json.dumps(rpcCmd))
 	r = json.loads(s.recv(8000000))
+	print(r)
 	if r["error"] != None:
 		return "send error: " + r["error"]
 	return "Sent. TXID: " + r["result"]["Txids"][0]
@@ -31,6 +60,12 @@ class lndrpcui:
 			type=gtk.MESSAGE_INFO,  buttons=gtk.BUTTONS_OK,message_format=txid)
 		d.run()
 		d.destroy()
+		
+	def gBal(self, widget, balWidget, rcvadrWidget):
+		bal = getBal()
+		balWidget.set_text("Balance: " + "{:,}".format(bal) + " (" + str(bal/100000000.0) + "BTC)")
+		adr = getAdr()
+		rcvadrWidget.set_text(adr)
 
 	def __init__(self):
 
@@ -43,22 +78,46 @@ class lndrpcui:
 		main_vbox.set_border_width(10)
 		window.add(main_vbox)
 
+		rcvFrame = gtk.Frame("Receive Address")
+		main_vbox.pack_start(rcvFrame, True, False, 0)
+#~ recvHbox
+		rcvhbox = gtk.HBox(False, 0)
+		rcvhbox.set_border_width(5)
+		rcvFrame.add(rcvhbox)
+		rcvLabel = gtk.Label("receive address here")
+		rcvLabel.set_selectable(True)
+		rcvhbox.pack_start(rcvLabel, False, False, 5)
+		
+		balFrame = gtk.Frame("balance")
+		main_vbox.pack_start(balFrame, True, False, 0)
+#~ balHbox
+		balhbox = gtk.HBox(False, 0)
+		balhbox.set_border_width(5)
+		balFrame.add(balhbox)
+		balLabel = gtk.Label("balance here")
+		refreshButton = gtk.Button("Refresh")
+		refreshButton.connect("clicked", self.gBal, balLabel, rcvLabel)
+		balhbox.pack_start(refreshButton,  False, False, 5)
+		balhbox.pack_end(balLabel,  False, False, 5)
+
+#~ adr / amt vbox
 		frame = gtk.Frame("send coins (satoshis)")
 		main_vbox.pack_start(frame, True, False, 0)
-  
+
 		vbox = gtk.VBox(False, 0)
 		vbox.set_border_width(5)
 		frame.add(vbox)
-  
+		
+ #~ adr / amt hbox 
 		hbox = gtk.HBox(False, 0)
 		vbox.pack_start(hbox, False, False, 5)
 		
 		sendButton = gtk.Button("Send")
 		vbox.pack_start(sendButton, False, False, 5)
-  #~ adrVbox
+#~ adrVbox
 		adrVbox = gtk.VBox(False, 0)
 		hbox.pack_start(adrVbox, True, True, 5)
-		adrLabel = gtk.Label("address")
+		adrLabel = gtk.Label("send to address")
 		adrLabel.set_alignment(0, 1)
 	  
 		adrVbox.pack_start(adrLabel, False, False, 0)
@@ -66,7 +125,7 @@ class lndrpcui:
 		adrEntry = gtk.Entry(50)
 		adrEntry.set_size_request(500, -1)
 		adrVbox.pack_start(adrEntry, True, True, 0)       
-  #~ amtVbox     
+#~ amtVbox     
 		amtVbox = gtk.VBox(False, 0)
 		hbox.pack_start(amtVbox, False, False, 5)
 
